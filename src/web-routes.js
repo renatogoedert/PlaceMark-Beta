67998cd6581040b3ad36b2331f8f089526e8add1
RGoedert <renato.goedert@gmail.com>
2023-03-16 15:20:49 +0000

    admin dashboard, list and del users

--- src/web-routes.js
+++ src/web-routes.js
@@ -2,6 +2,7 @@ import { accountsController } from "./controllers/accounts-controller.js";
 import { dashboardController } from "./controllers/dashboard-controller.js";
 import { aboutController } from "./controllers/about-controller.js";
 import { placemarkController } from "./controllers/placemark-controller.js";
+import { adminController } from "./controllers/admin-controller.js";
 
 export const webRoutes = [
   { method: "GET", path: "/", config: accountsController.index },
@@ -24,4 +25,6 @@ export const webRoutes = [
 
   { method: "GET", path: "/{param*}", handler: { directory: { path: "./public" } }, options: { auth: false } },
 
+  { method: "GET", path: "/admindashboard", config: adminController.index },
+  { method: "GET", path: "/admindashboard/deleteuser/{id}", config: adminController.deleteUser },
 ];
